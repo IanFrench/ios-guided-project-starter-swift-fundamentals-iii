1b2474fba8e2f91e11303e6888bf67f9fb96a86d
Ian French <igf2011@hotmail.com>
2020-03-19 13:33:54 -0400

    Update code for guided project

--- TurkeyCalculator/TurkeyCalculator/ViewController.swift
+++ TurkeyCalculator/TurkeyCalculator/ViewController.swift
@@ -19,12 +19,50 @@ class ViewController: UIViewController {
     // Actions
     
     @IBAction func calculateButtonPressed(_ sender: Any) {
+        guard let turkeyWeightString = turkeyWeightTextField.text else { return }
+        
+        guard let turkeyWeight = Double(turkeyWeightString) else {
+            print("Invalid Number")
+            return
+        }
+        
+        var durationinMinutes: Double
+        
+        if metricButton.isSelected {
+        durationinMinutes = cooktimeInKilos(turkeyWeight)
+        } else {
+        durationinMinutes = cooktimeInPounds(turkeyWeight)
+        }
+        let durationinHours = durationinMinutes / 60
+        cookTimeTextField.text = "\(durationinHours) hours"
+        
+        
         
     }
     
     @IBAction func metricButtonPressed(_ sender: Any) {
         
+        guard let button = sender as? UIButton else
+        { return }
+        button.isSelected.toggle()
+        
+        
     }
     
     // Helper functions
+    func cooktimeInPounds(_ weight: Double) -> Double {
+        let durationinMinutes = weight * 15.0
+        return durationinMinutes
+    }
+    func cooktimeInKilos (_ weight: Double) -> Double {
+        var durationinMinutes: Double
+        
+        if weight >= 4 {
+            durationinMinutes = 20 * weight + 90
+        } else {
+            durationinMinutes = 20 * weight + 70
+        }
+        return durationinMinutes
+    }
+    
 }
